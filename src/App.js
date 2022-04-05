5439a523b919dabb4d645592ff8a5e4fcb6bfc34
Joas-Assuncao <joassalvior@gmail.com>
2022-04-05 12:32:31 -0300

    Tirando códigos que não eram necessário

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import GlobalStyles from './styles/global';
 import Layout from './components/Layout';
 
 export default function App() {
-    // yarn add @babel/plugin-proposal-class-properties -D
-
     return (
         <>
             <GlobalStyles />
-            <Layout />
         </>
     );
 }
